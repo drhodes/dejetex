b7e9bacd1e1497220a59e7efc0ab38323dde5c52
Derek Rhodes <physci@gmail.com>
2023-12-27 12:02:58 -0500

    adds tests

---
+++ tests/generic_test.py
@@ -0,0 +1,22 @@
+from antlr4 import FileStream, CommonTokenStream
+from dejetex.DejeTexLexer import DejeTexLexer
+from dejetex.DejeTex import DejeTex
+from dejetex.DejeTexVisitor import DejeTexVisitor
+
+
+def generic_test(inner, name):
+    input_filename = f"./tests/deje-tests/{name}.deje"
+    input_stream = FileStream(input_filename)
+    lexer = DejeTexLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = DejeTex(stream)
+    inner(parser)
+
+def cmd_test(name):
+    def inner(parser):
+        tree = parser.program()
+        if parser.getNumberOfSyntaxErrors() > 0:
+            raise Exception("syntax errors")
+        else:
+            vinterp = DejeTexVisitor()
+            vinterp.visit(tree)
--- tests/test_vertical.py
+++ tests/test_vertical.py
@@ -1,23 +1,6 @@
-from antlr4 import FileStream, CommonTokenStream
-from dejetex.DejeTexLexer import DejeTexLexer
-from dejetex.DejeTex import DejeTex
-from dejetex.DejeTexVisitor import DejeTexVisitor
+from . generic_test import cmd_test
 
+# sanity test for syntax errors.
 
-def vertical_test(name):
-    input_filename = f"./tests/deje-tests/{name}.deje"  # Replace with your test filename
-    input_stream = FileStream(input_filename)
-    lexer = DejeTexLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = DejeTex(stream)
-    tree = parser.program()
-    if parser.getNumberOfSyntaxErrors() > 0:
-        print(parser)
-        raise Exception("syntax errors")
-    else:
-        vinterp = DejeTexVisitor()
-        vinterp.visit(tree)
-
-
-
-# def test_vertical(): vertical_test("vertical1")
+def test_vertical1(): cmd_test("vertical1")
+def test_vertical2(): cmd_test("vertical2")
