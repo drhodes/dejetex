531d2c7ee26bf56ede38f975ab9a6d0112b11423
Derek Rhodes <physci@gmail.com>
2023-12-30 23:23:09 -0500

    main function now runs environment checker

--- dejetex/main.py
+++ dejetex/main.py
@@ -1,20 +1,23 @@
 import sys
 from antlr4 import *
 from DejeTexLexer import DejeTexLexer
-from DejeTexParser import DejeTexParser
+from DejeTex import DejeTex
 from DejeTexVisitor import DejeTexVisitor
+from check_env import check
 
 def main(argv):
     input_stream = FileStream(argv[1])
     lexer = DejeTexLexer(input_stream)
     stream = CommonTokenStream(lexer)
-    parser = DejeTexParser(stream)
-    tree = parser.expr()
+    parser = DejeTex(stream)
+    tree = parser.program()
     if parser.getNumberOfSyntaxErrors() > 0:
         print("syntax errors")
     else:
-        vinterp = DejeTexVisitor()
-        vinterp.visit(tree)
+        # vinterp = DejeTexVisitor()
+        # vinterp.visit(tree)
+        check(tree, tree)
 
+        
 if __name__ == '__main__':
     main(sys.argv)
